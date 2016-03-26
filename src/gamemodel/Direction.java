3031a309631b8871e63a8f0fbfbc3753b490434d
Vándor Dávid <vandor.david@gmail.com>
2016-03-26 09:29:48 +0100

    Véleményezéseket kérek szépen a megoldásról! Köszi

--- src/gamemodel/Direction.java
+++ src/gamemodel/Direction.java
@@ -1,5 +1,18 @@
 package gamemodel;
 
 public enum Direction {
-	NORTH, SOUTH, EAST, WEST
+	NORTH, SOUTH, EAST, WEST;
+
+	Direction getOppositeDirection(Direction dir) {
+		switch (dir) {
+		case NORTH:
+			return SOUTH;
+		case SOUTH:
+			return NORTH;
+		case EAST:
+			return WEST;
+		default:
+			return EAST;
+		}
+	}
 }
--- src/gamemodel/MapElement.java
+++ src/gamemodel/MapElement.java
@@ -7,36 +7,49 @@ public abstract class MapElement {
 	private HashMap<Direction, MapElement> neighbours = new HashMap<Direction, MapElement>();
 	private Box box = null;
 
+	/* Getter for neighbour MapElement in a specific direction. */
 	public MapElement getNeighbour(Direction dir) {
 		return neighbours.get(dir);
 	}
 
+	/* Setter for neighbour MapElement in a specific direction. */
 	public void setNeighbour(Direction dir, MapElement neighbour) {
 		neighbours.put(dir, neighbour);
 	}
 
+	/* Getter for the box, that is on this MapElement, if any. */
 	public Box getBox() {
-		// na talán most
 		return box;
 	}
 
+	/*
+	 * Funcion for player arrival. Puts him back to the previous tile by
+	 * default.
+	 */
 	public void handlePlayerArrive(Direction dir, Player player) {
-		// TODO
+		player.leaveMapElement(this.getNeighbour(dir.getOppositeDirection(dir)));
 	}
 
+	/* Funcion for player leave. Does nothing by default */
 	public void handlePlayerLeave() {
-		// TODO
+
 	}
 
+	/* Funcion for projectile arrival. Does nothing by default */
 	public void handleProjectileArrive(Direction dir, Projectile projectile) {
-		// TODO
+
 	}
 
+	/*
+	 * Function for box putting down on this tile. Takes it back to the previous
+	 * tile by default.
+	 */
 	public void handleBoxPutDown(Direction dir, Box box) {
-		// TODO
+		box.leaveMapElement(this.getNeighbour(dir.getOppositeDirection(dir)));
 	}
 
+	/* Function for box picking up from this tile. Does nothing by default. */
 	public void handleBoxPickUp() {
-		// TODO
+
 	}
 }
