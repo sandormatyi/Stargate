5663cea823685cc0aa58594ee485b514a479a275
Roland Rosner <rosner.roland.bme@gmail.com>
2016-03-26 15:33:42 +0100

    Kommentek

--- src/gamemodel/Movable.java
+++ src/gamemodel/Movable.java
@@ -5,22 +5,22 @@ public abstract class Movable {
 	public Movable(MapElement position) {
 		this.position = position;
 	}
-//blabla
+	
+	//Moveable's position
 	protected MapElement position;
-
+	
+	//Set the position
 	public void setPosition(MapElement position) {
 		this.position = position;
 	}
-
+	
+	//Override in the descendants
 	public abstract void arriveOnMapElement(Direction dir, MapElement element);
-
+	
+	//Override in the descendants
 	public abstract void leaveMapElement(MapElement element);
 
+	//Override in the descendants
 	public void move() {
-		// TODO
-		this.leaveMapElement(position);
-		MapElement nextposition;
-		//nextposition = getNeighbourDirection(direction);
-		//arriveOnMapElement(direction,nextposition);
 	}
 }
