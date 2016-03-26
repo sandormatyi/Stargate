89352d6aeb44c6b8c3ebee808c7c4ee123242172
Roland Rosner <rosner.roland.bme@gmail.com>
2016-03-26 15:00:58 +0100

    Kommentek és függvénykiegészítések

--- src/gamemodel/Player.java
+++ src/gamemodel/Player.java
@@ -14,50 +14,59 @@ public class Player extends Movable {
 
 	@Override
 	public void arriveOnMapElement(Direction dir, MapElement element) {
-		// TODO
 		element.handlePlayerArrive(dir, this);
 	}
 
 	@Override
 	public void leaveMapElement(MapElement element) {
-		// TODO
-		position.handlePlayerLeave();
+		element.handlePlayerLeave();
 	}
 
+	//Call the super's move method
 	@Override
 	public void move() {
-		// TODO
+		super.move();
 	}
-
+	//Player is alive?
 	public boolean isAlive() {
 		return isAlive;
 	}
-
+	//Turn the player to a direction
 	public void turn(Direction direction) {
-		// TODO
 		this.direction = direction;
 	}
-
+	//Shoot a projectile
 	public void shoot() {
 		// Ezt nem tudom hogy kéne egyelõre
 	}
-
+	//Pick up a box from the next mapElement
 	public void pickUpBox() {
-		// TODO
-		MapElement nextposition;
-		nextposition = position.getNeighbour(direction);
-		nextposition.getBox();
-		box.leaveMapElement(nextposition);
+		//If the player already has a box, then return
+		if (this.box != null) return;
+		//Otherwise get the box from the next element
+		MapElement nextposition = position.getNeighbour(direction);
+		//If there is not nextPosition, then return.
+		if (nextposition == null) return;
+		this.box = nextposition.getBox();
+		//If we get a box, then set it's position to null.
+		if (this.box != null) box.leaveMapElement(nextposition);
 	}
-
+	
+	//Put down a box to the next mapElement
 	public void putDownBox() {
-		// TODO
-		MapElement nextposition;
-		nextposition = position.getNeighbour(direction);
-		nextposition.getBox();
+		MapElement nextposition = position.getNeighbour(direction);
+		//If there is not nextPosition, then return.
+		if (nextposition == null) return;
+		Box nextPositionBox = nextposition.getBox();
+		//If the nextMapElement already has a box, then return
+		if (this.box != null) return;
+		//Otherwise put down the player's box
 		box.arriveOnMapElement(direction, nextposition);
+		//Set our box to null
+		this.box = null;
 	}
-
+	
+	//Set the isAlive to false
 	public void die() {
 		// TODO
 		this.isAlive = false;
