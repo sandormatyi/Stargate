ca526a62a413b8f2b34e7469f69eb304581f14aa
Roland Rosner <rosner.roland.bme@gmail.com>
2016-03-27 19:51:39 +0200

    KOmmentek

--- src/gamemodel/Movable.java
+++ src/gamemodel/Movable.java
@@ -1,26 +1,35 @@
 package gamemodel;
 
 public abstract class Movable {
-	//
+	/*
+	 *Moveable's position
+	*/	
+	protected MapElement position;
+	
 	public Movable(MapElement position) {
 		this.position = position;
 	}
-	
-	//Moveable's position
-	protected MapElement position;
-	
-	//Set the position
+
+	/*
+	 *Set the position
+	*/
 	public void setPosition(MapElement position) {
 		this.position = position;
 	}
 	
-	//Override in the descendants
+	/*
+	 *Override in the descendants
+	*/
 	public abstract void arriveOnMapElement(Direction dir, MapElement element);
 	
-	//Override in the descendants
+	/*
+	 *Override in the descendants
+	*/
 	public abstract void leaveMapElement(MapElement element);
 
-	//Override in the descendants
+	/*
+	 *Override in the descendants
+	*/
 	public void move() {
 	}
 }
