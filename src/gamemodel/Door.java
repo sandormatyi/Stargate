7087bded5dba4d17c3f0181d6d48e9e4152d9746
sandormatyi <sandormatyi@gmail.com>
2016-03-27 21:16:05 +0200

    Door manikűrözés

--- src/gamemodel/Door.java
+++ src/gamemodel/Door.java
@@ -4,23 +4,29 @@ public class Door extends MapElement {
 
 	private boolean isOpened = false;
 
+	/*
+	 * Opens or closes the door. When the door closes, it destroys the box if
+	 * there was on on the field
+	 */
 	public void setOpened(boolean isOpened) {
-		// TODO
 		this.isOpened = isOpened;
+
+		if (box != null)
+			box.respawn();
 	}
 
 	/*
-	 * Player arrives on Door
-	 * if the door is opened the Player arrives on the Door MapElement
-	 * if the door is closed the Player can not arrives on the Door
+	 * Player arrives on Door if the door is opened the Player arrives on the
+	 * Door MapElement if the door is closed the Player can not arrives on the
+	 * Door
 	 */
 	@Override
 	public void handlePlayerArrive(Direction dir, Player player) {
-		// TODO
-		if (isOpened == true)
+		if (isOpened) {
 			player.setPosition(this);
-		else
-			player.setPosition(this.getNeighbour(dir));
+		} else {
+			super.handlePlayerArrive(dir, player);
+		}
 	}
 
 	/*
@@ -32,29 +38,29 @@ public class Door extends MapElement {
 	}
 
 	/*
-	 * Projectile arrives
-	 * if the Door is opened the projectile goes through it to the next MapElement
-	 * if the door is closed the projectile stops on the Door, waiting for the gc
+	 * Projectile arrives if the Door is opened the projectile goes through it
+	 * to the next MapElement if the door is closed the projectile stops on the
+	 * Door, waiting for the gc
 	 */
 	@Override
 	public void handleProjectileArrive(Direction dir, Projectile projectile) {
-		// TODO
 		projectile.setPosition(this);
-		if (isOpened == true)
-			projectile.move();
+
+		if (!isOpened)
+			projectile.destroy();
 	}
 
 	/*
-	 *  Put down a box
-	 *  if the door is closed the box position is the player's position
+	 * Put down a box if the door is closed the box position is the player's
+	 * position
 	 */
 	@Override
 	public void handleBoxPutDown(Direction dir, Box box) {
-		// TODO
-		if (isOpened = true)
-			box.position = this;
-		else
-			box.position = this.getNeighbour(dir);
+		if (isOpened) {
+			box.setPosition(this);
+		} else {
+			super.handleBoxPutDown(dir, box);
+		}
 	}
 
 	@Override
