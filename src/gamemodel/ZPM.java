eee732ee7e4b9822e79d79687a44e8f5055d4b8b
evettke7 <szalisznyo.ivett@gmail.com>
2016-03-27 14:12:21 +0200

    ZPM osztály

--- src/gamemodel/ZPM.java
+++ src/gamemodel/ZPM.java
@@ -2,7 +2,15 @@ package gamemodel;
 
 public class ZPM {
 
+	/*
+	 * We need to create "game" when the game starts
+	 * increments the score variable of Game class
+	 * if it is equal to 5 (or 10, whatever) the game stops
+	 */
 	public void handlePickUp() {
 		// TODO
+		game.incrementScore();
+		if (game.getScore() == 5)
+			game.stop();
 	}
 }
