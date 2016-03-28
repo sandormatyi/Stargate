7830d922156074388fb70de5a61671fd6259798d
Roland Rosner <rosner.roland.bme@gmail.com>
2016-03-28 22:54:04 +0200

    C1re ZPM kerülés miatt egyszerűsítés az útra lépésben

--- src/test/PlayerMoveRoadTest.java
+++ src/test/PlayerMoveRoadTest.java
@@ -11,30 +11,14 @@ class PlayerMoveRoadTest extends Test {
 
 	@Override
 	public void setUp() {
-		// Turn WEST
-		controller.moveOrTurnPlayer(Direction.WEST);
-		// Turn EAST
-		controller.moveOrTurnPlayer(Direction.EAST);
-		//Pick up box
-		controller.pickUpBox();
-		// MOve EAST
-		controller.moveOrTurnPlayer(Direction.EAST);
-		// Turn WEST
-		controller.moveOrTurnPlayer(Direction.WEST);
-		//Put down box - Open Door
-		controller.putDownBox();
-		// Turn EAST
-		controller.moveOrTurnPlayer(Direction.EAST);
-		// MOve EAST
-		controller.moveOrTurnPlayer(Direction.EAST);		
-		// Turn NORTH
-		controller.moveOrTurnPlayer(Direction.NORTH);
+		// Turn SOUTH
+		controller.moveOrTurnPlayer(Direction.SOUTH);
 	}
 
 	@Override
 	public void run() {
-		// Move NORTH
-		controller.moveOrTurnPlayer(Direction.NORTH);
+		// Move SOUTH
+		controller.moveOrTurnPlayer(Direction.SOUTH);
 	}
 
 }
