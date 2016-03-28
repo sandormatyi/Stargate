970466a3e56d454a78a5135e64967bab2815cc83
csokamate <csoka.mate@gmail.com>
2016-03-28 22:47:28 +0200

    boxPutDown gap, road, scale

--- src/test/BoxPutDownGapTest.java
+++ src/test/BoxPutDownGapTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import controller.Controller;
+import gamemodel.Direction;
 
 class BoxPutDownGapTest extends Test {
 
@@ -10,14 +11,22 @@ class BoxPutDownGapTest extends Test {
 
 	@Override
 	public void setUp() {
-		// TODO Auto-generated method stub
-
+		// Turn south
+		controller.moveOrTurnPlayer(Direction.SOUTH);
+		// Step south
+		controller.moveOrTurnPlayer(Direction.SOUTH);
+		// Turn north
+		controller.moveOrTurnPlayer(Direction.NORTH);
+		// Pick up box
+		controller.pickUpBox();
+		// Step north
+		controller.moveOrTurnPlayer(Direction.NORTH);
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		// Put down box
+		controller.putDownBox();
 	}
 
 }
--- src/test/BoxPutDownScaleTest.java
+++ src/test/BoxPutDownScaleTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import controller.Controller;
+import gamemodel.Direction;
 
 class BoxPutDownScaleTest extends Test {
 
@@ -10,14 +11,24 @@ class BoxPutDownScaleTest extends Test {
 
 	@Override
 	public void setUp() {
-		// TODO Auto-generated method stub
-
+		// Turn south
+		controller.moveOrTurnPlayer(Direction.SOUTH);
+		// Step south
+		controller.moveOrTurnPlayer(Direction.SOUTH);
+		// Turn north
+		controller.moveOrTurnPlayer(Direction.NORTH);
+		// Pick up box
+		controller.pickUpBox();
+		// Step north
+		controller.moveOrTurnPlayer(Direction.NORTH);
+		// Turn west
+		controller.moveOrTurnPlayer(Direction.WEST);
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		// Put down box
+		controller.putDownBox();
 	}
 
 }
