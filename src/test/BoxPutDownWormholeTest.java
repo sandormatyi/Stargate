c09c0b460296f24555bf27591dd9cd54df311b07
Roland Rosner <rosner.roland.bme@gmail.com>
2016-03-28 22:31:40 +0200

    Doboz lerakás portálon keresztül

--- src/test/BoxPutDownWormholeTest.java
+++ src/test/BoxPutDownWormholeTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import controller.Controller;
+import gamemodel.Direction;
 
 class BoxPutDownWormholeTest extends Test {
 
@@ -9,14 +10,27 @@ class BoxPutDownWormholeTest extends Test {
 	}
 
 	@Override
-	public void setUp() {
-		// TODO Auto-generated method stub
-
+	public void setUp() {	
+		// Move WEST
+		controller.moveOrTurnPlayer(Direction.WEST);
+		// Turn EAST
+		controller.moveOrTurnPlayer(Direction.EAST);
+		//Pick up box
+		controller.pickUpBox();
+		// Turn NORTH
+		controller.moveOrTurnPlayer(Direction.NORTH);
+		//Shoot projectile
+		controller.shoot();
+		// Turn SOUTH
+		controller.moveOrTurnPlayer(Direction.SOUTH);
+		//Shoot projectile - Open Stargate
+		controller.shoot();
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		//Put down box - Stargate
+		controller.putDownBox();
 
 	}
 
