4d1c4b772188bea3139828919ab8d202669df934
sandormatyi <sandormatyi@gmail.com>
2016-03-28 12:33:24 +0200

    SkeletonLogger osztály kiegészítése

--- src/debug/SkeletonLogger.java
+++ src/debug/SkeletonLogger.java
@@ -1,43 +1,76 @@
 package debug;
 
 public class SkeletonLogger {
-	
+
 	/*
 	 * The current log depth
 	 */
 	private static int logDepth = 0;
-	
+
+	/*
+	 * The logger only prints to the output if this field is true
+	 */
+	private static boolean printEnabled = false;
+
 	/*
 	 * Increments the log depth
 	 */
 	private static void incrementLogDepth() {
 		logDepth++;
 	}
-	
+
 	/*
 	 * Decrements the log depth
 	 */
 	private static void decrementLogDepth() {
 		logDepth--;
-		
+
 		if (logDepth < 0) {
 			System.err.println(SkeletonLogger.class.getSimpleName() + ": logDepth cannot be negative!");
 		}
 	}
-	
+
 	/*
-	 * Prints the textual representation of a function call to the standard output
+	 * Enables printing to the standard output
+	 */
+	public static void enablePrint() {
+		printEnabled = true;
+	}
+
+	/*
+	 * Disables printing to the standard output
+	 */
+	public static void disablePrint() {
+		printEnabled = false;
+	}
+
+	/*
+	 * Prints the textual representation of a function call to the standard
+	 * output
 	 */
 	public static void callFunction(Object caller, String functionName) {
+		if (!printEnabled)
+			return;
+
+		for (int i = 0; i < logDepth; i++)
+			System.out.print("\t");
+
 		System.out.println(caller.getClass().getSimpleName() + "." + functionName);
-		
+
 		incrementLogDepth();
 	}
-	
+
 	/*
-	 * Prints the textual representation of returning from a function to the standard output
+	 * Prints the textual representation of returning from a function to the
+	 * standard output
 	 */
 	public static void returnFromFunction(Object returnValue) {
+		if (!printEnabled)
+			return;
+
+		for (int i = 0; i < logDepth; i++)
+			System.out.print("\t");
+
 		if (returnValue == null) {
 			System.out.println("return");
 		} else {
