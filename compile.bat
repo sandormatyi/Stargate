1c680b7605bf85bd42ad5fb1071a667f3eee05a6
sandormatyi <sandormatyi@gmail.com>
2016-04-22 15:49:54 +0200

    Compile script fejlesztés

--- compile.bat
+++ compile.bat
@@ -3,13 +3,16 @@ set OLDDIR=%CD%
 
 cd /d %~dp0
 
-IF NOT EXIST bin mkdir bin
+echo --- Cleaning project... ---
+IF EXIST bin rmdir /s/q bin
+mkdir bin
+
+echo --- Compiling... ---
 javac -encoding UTF-8 -d .\bin .\src\controller\*.java .\src\debug\*.java .\src\gamemodel\*.java .\src\gamemodel\events\*.java .\src\test\*.java .\src\userinterface\*.java
 
-IF NOT EXIST bin/res mkdir bin/res
-robocopy ./res ./bin /E
+echo --- Copying resources... ---
+robocopy ./res ./bin /E >nul
 
-echo.
 echo --- Compilation finished ---
 echo.
 chdir /d %OLDDIR%
