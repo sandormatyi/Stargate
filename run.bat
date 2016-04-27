bc33da0ab08ee8ddc78cd5bf6ea3a49375e4f1c6
Sándor Mátyás <sandormatyi@gmail.com>
2016-04-27 11:07:04 +0200

    run.bat módosítás beadás előtt

--- run.bat
+++ run.bat
@@ -2,10 +2,26 @@
 set OLDDIR=%CD%
 
 cd /d %~dp0
-cd .\bin
+cd .\bin 2>nul
+if errorlevel==1 (
+	echo Futtatas elott forditsd le a forraskodot!
+	echo.
+	pause
+	goto :end)
+	
 
-chcp 65001
-java userinterface.Main
+chcp 65001 >nul
+echo Ha Command Promptbol futtatod a szkriptet, kerlek válts Lucida Console betutipusra!
+echo.
+pause
+echo.
+echo Windows 7 és korabbi rendszereken a Command Prompt Unicode támogatasanak hianya miatt a kepernyore kiirt kimenet hibas lehet (ekezetet tartalmazo sorok vegen a sorvegi karakterek megismetlodnek). Ez azonban nem erinti a java altal irt és olvasott szoveget, igy a tesztek validalasat sem.
+echo.
+pause
+echo.
+"%JAVA_HOME%/bin/java" userinterface.Main
 
 echo.
+
+:end
 chdir /d %OLDDIR%
