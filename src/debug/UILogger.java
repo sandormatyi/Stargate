22172ae0f95a3209103078f19b020991505f8edc
sandormatyi <sandormatyi@gmail.com>
2016-04-28 13:47:15 +0200

    Kisebb kozmetika: izgalmasabb pálya, szebb doboz, kevesebb kimenet!

--- src/debug/UILogger.java
+++ src/debug/UILogger.java
@@ -20,6 +20,9 @@ public class UILogger {
 	 * Prints the given text to the standard output
 	 */
 	public static void log(String output) {
-		outStream.println("--- " + Thread.currentThread().getName() + " --- " + output);
+		/*
+		 * outStream.println("--- " + Thread.currentThread().getName() + " --- "
+		 * + output);
+		 */
 	}
 }
